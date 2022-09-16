d1de03f09f00f56c76bec9d8445a1caefecc6c0f
sameerkhan001 <sameerkhan.n@syncfusion.com>
2022-09-16 20:18:05 +0530

    EJDOTNETCORE-3951 Changed the output file name

--- LeaseAgreement/LeaseAgreement/Program.cs
+++ LeaseAgreement/LeaseAgreement/Program.cs
@@ -1,8 +1,8 @@
 ﻿using LeaseAgreement;
 using LeaseAgreement.Models;
 
-Console.WriteLine("sample.pdf");
-FileStream fs = new FileStream("MonthtoMonth.pdf", FileMode.Create);
+Console.WriteLine("SampleProject.pdf");
+FileStream fs = new FileStream("LeaseAgreement.pdf", FileMode.Create);
 LeaseModel model = LeaseDataSource.GetMonthLeaseDetails();
 LeaseDocument document = new LeaseDocument(model);
 document.GeneratePdf(fs);
